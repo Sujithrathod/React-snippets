7a05fbc9a8a3def635a0d03dd1c7c7cff50cc1c7
sujithrathodm <sujithrathodm2004@gmail.com>
2025-06-07 01:28:09 +0530

    blog-3 added

---
+++ blog-3/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState ,useEffect,useRef} from 'react'
+import './App.css'
+
+function App() {
+  const [data,setData] = useState(null);
+  useEffect(() =>{
+    fetch("https://jsonplaceholder.typicode.com/todos/2")
+    .then((res) => res.json())
+    .then((result) => setData(result))
+  },[data])
+  
+  return (
+    <div>
+      <h1>{data ? data.title : <p>Loading....</p>}</h1>
+    </div>
+  )
+}
+
+export default App
+
+// fetching data using useEffect
+// useRef -> it gives you a way to hang onto something (like a DOM element) that doesn't get wiped out when your component re-renders
