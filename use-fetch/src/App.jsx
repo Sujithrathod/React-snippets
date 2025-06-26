2ea88db40cfed24dcc1d3042f03e52835bcbe44e
sujithrathodm <sujithrathodm2004@gmail.com>
2025-06-26 15:35:43 +0530

    use-fetch file added

---
+++ use-fetch/src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+
+export default function App() {
+  const [count,setCount] = useState(1);
+  const [response,setResponse] = useState({});
+  const [loading,setLoading] = useState(true)
+
+  async function fetchData(count) {
+    setLoading(true);
+    const data = await fetch("https://jsonplaceholder.typicode.com/todos/" + count);
+    const jsonData = await data.json();
+    setResponse(jsonData);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData(count)
+  },[count])
+
+  return (
+    <>
+       <p>{loading? "Loading" :  JSON.stringify(response)}</p>
+      <button onClick={() => setCount(1)}>1st</button>
+      <button onClick={() => setCount(2)}>2nd</button>
+      <button onClick={() => setCount(3)}>3rd</button>
+    </>
+  )
+}
